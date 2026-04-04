c862d7f6aa4bcfee97150df11b3700b0cc3f73a4
keepcodingria <roza.animesh@gmail.com>
2026-04-04 15:39:00 +0530

    Add files via upload

---
+++ prims.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<climits>
+using namespace std; 
+
+#define MAX 100
+
+int minKey(int vertices, int key[],bool mst[])
+{
+    int min=INT_MAX;
+    int minVertex;
+    
+    for(int v=0;v<vertices;v++){
+        if(!mst[v] && key[v]<min)
+        {
+            min=key[v];
+            minVertex=v;
+        }
+    }
+    return minVertex;
+}
+
+void prims(int graph[MAX][MAX],int vertices)
+{
+    bool mst[MAX];
+    int parent[MAX];
+    int key[MAX];
+
+    for(int v=0;v<vertices;v++)
+    {
+       key[v]=INT_MAX;
+       mst[v]=false;
+    }
+
+    key[0]=0;
+    parent[0]=-1;
+
+  for(int edges=0;edges<vertices-1;edges++)
+  {
+    int u=minKey(vertices,key,mst);
+    mst[u]=true;
+  
+    for(int v=0;v<vertices;v++)
+    {
+        if(graph[u][v] && !mst[v]&& graph[u][v]<key[v])
+        {
+          key[v] = graph[u][v];
+            parent[v]=u;
+        }
+    }
+  }
+
+cout<<"edge:weight"<<endl;
+for(int i=1;i<vertices;i++) // since parent of 0 is -1 , start from i=1
+{
+    cout<<parent[i]<<"-"<<i<< ": "<< graph[i][parent[i]]<<endl;
+}
+}
+
+int main()
+{
+   int graph[MAX][MAX];
+   int vertices;
+
+   cout<<"enter no. of vertices: "<<endl;
+   cin>>vertices;
+   cout<<" enter the edges (enter 0 if no edge exist) :"<<endl;
+
+   for(int i=0;i<vertices;i++)
+   {
+    for(int j=0;j<vertices;j++)
+    {
+       cin>> graph[i][j];
+    }
+   }
+
+   prims(graph,vertices);
+}
+
